1f80393be09093031e5bd94e5a2cbe71e4250b0b
Halil Ibrahim Disli <halil.disli@outlook.com>
2024-03-11 16:47:03 +0300

    API Kullanımına kadar olan kısım part2.

--- lib/repository/ogretmenler_repository.dart
+++ lib/repository/ogretmenler_repository.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ogrenci_app/services/data_service.dart';
+
+import '../models/ogretmen.dart';
 
 class OgretmenlerRepository extends ChangeNotifier{
 
@@ -7,18 +10,20 @@ class OgretmenlerRepository extends ChangeNotifier{
     Ogretmen("Faruk", "Yılmaz", 18, "Erkek"),
     Ogretmen("Semiha", "Çelik", 28, "Kadın"),
   ];
+  final DataService dataService;
+  OgretmenlerRepository(this.dataService);
+
+  void indir(){
+    Ogretmen ogretmen=dataService.ogretmenIndir();
+
+
+    ogretmenler.add(ogretmen);
+    notifyListeners();
+  }
 
 }
 
 final ogretmenlerProvider=ChangeNotifierProvider((ref) {
-  return OgretmenlerRepository();
+  return OgretmenlerRepository(ref.watch(dataServiceProvider));
 });
 
-class Ogretmen{
-  String ad;
-  String soyad;
-  int yas;
-  String cinsiyet;
-
-  Ogretmen(this.ad, this.soyad, this.yas, this.cinsiyet);
-}
